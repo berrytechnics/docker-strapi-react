563712529b9ed85279beff7c821dcca9064e8014
Kyle Berry <berrytechnics@gmail.com>
2023-04-17 20:49:32 -0500

    remove stock react files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [devices,setDevices] = useState("");
+  if(devices.length==0) setDevices("This is the boilerplate for a dockerized app using strapi and react.");
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         {devices}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
